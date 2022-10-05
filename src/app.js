fce4ca41c155c6565393a8a121c608f5ed9b7ed8
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-10-05 07:59:34 -0300

    upload dos arquivos da api linkada na nuvem

---
+++ src/app.js
@@ -0,0 +1,79 @@
+/********************************************************************
+OBJETIVO: PROJETO INTEGRADO ENTRE PWBE E PWFE
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 15/09/2022
+*********************************************************************/
+const express = require(`express`)
+const app = express()
+const cors = require(`cors`)
+const bodyParser = require(`body-parser`)
+const { getCurso } = require(`../module/cursos.js`)
+const { getAlunoPorCurso, getAlunoPorStatus, getAlunoPorMatricula, getDisciplinaPorAluno } = require(`../module/alunos.js`)
+
+app.use((request, response, next) => {
+    response.header(`Access-Control-Allow-Origin`, `*`)
+    response.header(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, OPTIONS`)
+    app.use(cors())
+    next()
+})
+
+// EndPoint para filtrar os cursos
+app.get(`/.netlify/functions/api/cursos`, cors(), async (request, response, next) => {
+    let cursos = getCurso()
+
+    if (cursos) {
+        return response.status(200).json(cursos)
+    }
+    return response.status(404)
+})
+
+// EndPoint para filtrar os alunos pelo curso
+app.get(`/.netlify/functions/api/alunos/`, cors(), async (request, response, next) => {
+    let curso = request.query.curso
+    let alunos = getAlunoPorCurso(curso)
+
+    if (alunos) {
+        return response.status(200).json(alunos)
+    }
+    return response.status(404)
+})
+
+// EndPoint para filtrar os alunos pelo status
+app.get(`/.netlify/functions/api/alunos/curso/`, cors(), async (request, response, next) => {
+    let curso = request.query.curso
+    let status = request.query.filtro
+    let alunos = getAlunoPorCurso(curso)
+    
+    if (status != undefined) {
+        alunos = await getAlunoPorStatus(alunos, status)
+    }
+
+    if (alunos) {
+        return response.status(200).json(alunos)
+    }
+    return response.status(404)
+})
+
+//EndPoint para filtrar um aluno pela matrícula
+app.get(`/.netlify/functions/api/aluno/:matricula`, cors(), async (request, response, next) => {
+    let matricula = request.params.matricula
+    let aluno = getAlunoPorMatricula(matricula)
+
+    if (aluno) {
+        return response.status(200).json(aluno)
+    }
+    return response.status(400)
+})
+
+// EndPoint para filtrar as disciplinas pelo aluno
+app.get(`/.netlify/functions/api/disciplinas/aluno/:matricula`, cors(), async (request, response, next) => {
+    let matricula = request.params.matricula
+    let disciplinas = getDisciplinaPorAluno(matricula)
+
+    if (disciplinas) {
+        return response.status(200).json(disciplinas)
+    }
+    return response.status(404)
+})
+
+module.exports = app
